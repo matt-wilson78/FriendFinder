2ba52724847f59c5df58e4d81734f0f126a685de
Matt Wilson <matt@firstflightagency.com>
2019-11-11 15:39:44 -0500

    getting a 500 error on survey submission, cannot read property 'reduce' of undefined. Working on figuring it out. Feels like i'm close

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.use(express.static("app/public"));
-
-app.use(express.urlencoded({
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(express.json());
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+    type: "application/vnd.api+json"
+}));
+
+app.use(express.static("app/public"));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
