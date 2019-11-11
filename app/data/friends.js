d082020e41b3bb1a9bcd9ea50468dee8aa9fdcc1
Matt Wilson <matt@firstflightagency.com>
2019-11-11 12:07:26 -0500

    Fixed survey page so that there's a blank option at top of choices, added starter friends to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+var friends = [{
+        name: "Roger Waters",
+        photo: "https://www.rollingstone.com/wp-content/uploads/2018/06/roger-waters-qa-c2059a03-f6a2-4e51-ae2a-a21715356af3.jpg?resize=900,600&w=1200",
+        answers: [5, 1, 3, 2, 5, 4, 2, 2, 4, 1]
+    },
+    {
+        name: "David Gilmour",
+        photo: "https://www.telegraph.co.uk/content/dam/music/2015-09/16-2015/david-gilmour-lead.jpg?imwidth=1400",
+        answers: [5, 2, 4, 1, 5, 2, 3, 1, 4, 2]
+    },
+    {
+        name: "Nick Mason",
+        photo: "https://www.rollingstone.com/wp-content/uploads/2018/12/nick-mason-saucerful-of-secrets.jpg?resize=900,600&w=1200",
+        answers: [5, 4, 5, 3, 5, 4, 2, 1, 5, 5]
+    },
+    {
+        name: "Rick Wright",
+        photo: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/4-rick-wright-pink-floyd-best-actors.jpg",
+        answers: [4, 1, 3, 4, 3, 2, 2, 1, 5, 2]
+    }
+]
+
+module.exports = friends;
